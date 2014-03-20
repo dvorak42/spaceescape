aa3cc616088b7a7b266071a13382e06aa58a5968
Steven Valdez <dvorak42@mit.edu>
2014-03-20 14:35:02 -0400

    Adding libgdx code

---
+++ spaceescape-desktop/src/com/se/spaceescape/Main.java
@@ -0,0 +1,15 @@
+package com.se.spaceescape;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "spaceescape";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new SpaceEscapeGame(), cfg);
+	}
+}
